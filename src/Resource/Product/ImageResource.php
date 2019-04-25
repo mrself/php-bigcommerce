51155d562ea5ce893a9b5c4a78d7022a34181757
Lightman <yaruk.my@gmail.com>
2019-04-25 16:31:43 +0300

    add product image resource

---
+++ src/Resource/Product/ImageResource.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\Bigcommerce\Resource\Product;
+
+use Mrself\Bigcommerce\Resource\AbstractResource;
+
+class ImageResource extends AbstractResource
+{
+    protected $namespaceSource = ['product', 'image'];
+}
